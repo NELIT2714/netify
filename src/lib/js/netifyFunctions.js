c71c922590b234e618a4f3659ee97aa3b1ecab51
NELIT2714 <nelit2714@gmail.com>
2025-01-14 00:39:07 +0100

    feat: create function for work with API

--- src/lib/js/netifyFunctions.js
+++ src/lib/js/netifyFunctions.js
@@ -1,12 +1,11 @@
 import api from "./api.js";
 
-export const getNetworkInfo = async (ipAddress, subnetPrefix) => {
+export const getNetworkDetails = async (ipAddress, subnetPrefix) => {
 	try {
 		const response = await api.post("/v1/network/details/", {
 			ip_address: ipAddress,
 			subnet_mask_prefix: subnetPrefix
 		})
-		console.log(response);
 		return response.data
 	} catch (err) {
 		console.error(err);
