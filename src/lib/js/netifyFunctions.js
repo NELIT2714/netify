4453c968efa8ddc747aa257bc7fb5b725193c5f0
NELIT2714 <nelit2714@gmail.com>
2025-01-14 09:41:13 +0100

    feat: create new Netify function for work with Netify API

--- src/lib/js/netifyFunctions.js
+++ src/lib/js/netifyFunctions.js
@@ -11,3 +11,39 @@ export const getNetworkDetails = async (ipAddress, subnetPrefix) => {
 		console.error(err);
 	}
 }
+
+export const convertPrefixToMask = async (subnetPrefix) => {
+	try {
+		const response = await api.get(`/v1/mask/prefix/${subnetPrefix}/`)
+		return response.data
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export const convertMaskToPrefix = async (subnetMask) => {
+	try {
+		const response = await api.get(`/v1/mask/ip/${subnetMask}/`)
+		return response.data
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export const convertIPToBinary = async (ipAddress) => {
+	try {
+		const response = await api.get(`/v1/ip/bin/${ipAddress}/`)
+		return response.data
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export const convertBinaryToIP = async (binaryIPAddress) => {
+	try {
+		const response = await api.get(`/v1/ip/dec/${binaryIPAddress}/`)
+		return response.data
+	} catch (err) {
+		console.error(err);
+	}
+}
